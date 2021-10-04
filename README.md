793e951cafb5d8d2fb73a891ea55583f081c798f
solfer <lalo200195@gmail.com>
2021-10-04 11:29:48 -0500

    Primeros cambios

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # curso-practico-js-platzi
 Curso basico de javascript en platzi
+
+...
+
+## Taller numero 1 Figuras geometricas
+
+- Primer paso: definir las formulas
+- Segundo paso: Implementar las formulas en javascript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar javascript para HTML
---
+++ figuras.js
@@ -0,0 +1,26 @@
+console.log("Hello world");
+
+//Codigo del cuadrado
+console.group("Cuadrado"); //Sirve para agrupar en consola
+const  ladoCuadrado = 3;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado );
+console.groupEnd();
+
+
+//Codigo del triangulo
+console.group("Triangulo");
+const  lado1Trinagulo = 3;
+console.log("El lado 1 del trinagulo mide: " + lado1Trinagulo + " cm");
+
+const  lado2Trinagulo = 5;
+console.log("El lado 2 del trinagulo mide: " + lado2Trinagulo + " cm");
+
+const  lado3Trinagulo = 2;
+console.log("El lado 3 del trinagulo mide: " + lado3Trinagulo + " cm");
+console.groupEnd();
